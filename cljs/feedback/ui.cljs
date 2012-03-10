1be7b8de02c3e79e6b9e9370e73deda286f7fa00
Jan Krüger <jan.krueger@gmail.com>
2012-03-10 14:10:48 +0100

    fixed ui to be continously updating

--- cljs/feedback/ui.cljs
+++ cljs/feedback/ui.cljs
@@ -1,6 +1,6 @@
 (ns feedback.ui
-  (:require [goog.dom :as gdom]
-            [crate.core :as crate]
+  (:require [goog.dom      :as gdom]
+            [crate.core    :as crate]
             [fetch.remotes :as remotes])
   (:require-macros [fetch.macros :as fm]))
 
@@ -13,28 +13,27 @@
   (.log js/console (apply s objs)))
 
 (defn build-dom [feedbacks]
-  (log "build")
   [:div#feedbacks
    [:h1 (str "Feedbacks:")]
    (let [trace (first feedbacks)]
-     (log trace)
-     (for [iteration (partition-by first trace)]
+     (for [iteration (partition-by :iteration trace)]
        [:div {:style "display:inline-block"}
         [:ul
-         (for [[iter-id let-id binding-id var value] iteration]
-           [:li (str var ": " value)])]]))])
+         (for [{:keys [var value]} iteration]
+           [:li (str (s var) ": " (s value))])]]))])
 
 (defn update-state []
   (fm/letrem [feedbacks (watch-feedbacks)]
-             (let [wrapper (gdom/$ "wrapper")
-                   child (-> feedbacks
-                             build-dom
-                             crate/html)]
-               (gdom/removeChildren wrapper)
-               (gdom/appendChild wrapper child))))
+    (let [wrapper (gdom/$ "wrapper")
+          child   (-> feedbacks
+                      build-dom
+                      crate/html)]
+      (gdom/removeChildren wrapper)
+      (gdom/appendChild wrapper child)
+      (update-state))))
 
 (defn ^:export init []
   (loop []
+    (log "bla")
     (update-state)
-;    (recur)
-))
+    (log "foo")))
